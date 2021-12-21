71b1b570f4c7d10b80ef874441af098df1153dad
liqian634126236 <634126236@qq.com>
2021-12-21 20:57:44 +0800

    First Commit

---
+++ test_upload/test_upload/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test_upload
+//
+//  Created by qian.li4 on 2021/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test_upload/test_upload/test_uploadApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_uploadApp.swift
+//  test_upload
+//
+//  Created by qian.li4 on 2021/12/21.
+//
+
+import SwiftUI
+
+@main
+struct test_uploadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
